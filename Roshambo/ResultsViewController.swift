fd4549ba9de9de8103e50c92f797683555ed2f3e
Himanshu Yadav <himsonly@gmail.com>
2015-08-23 09:22:48 -0400

    Added logic for a tie

--- Roshambo/ResultsViewController.swift
+++ Roshambo/ResultsViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
-    var userChoice:String!
+    // Mark: Outlets
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var resultText: UILabel!
+    
+    // Mark: Choices
+    var userChoice: String!
+    var opponentChoice: String!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let choices = ["Rock", "Paper", "Scissors"]
+        let randomIndex = Int(arc4random() % 3)
+        opponentChoice = choices[randomIndex]
+        
+        displayUI()
+    }
+    
+    
+    private func displayUI() {
+        var imageName: String
+        var text: String
+        var youWon: Bool = false
+        let matchup = "\(userChoice) vs. \(opponentChoice)"
+        
+        if userChoice == opponentChoice {
+            resultImage.image = UIImage(named: "tie")
+            resultText.text = "It's a tie!"
+            return
+        }
+    }
 
 }
